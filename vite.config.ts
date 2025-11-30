5315c89ed86f9e6a8b34f487353338c78d54e506
nilesh-kasar <nilesh.kasar@centralogic.net>
2025-11-30 13:26:17 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,11 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://rentdirect-dev.onrender.com',
+        target: 'http://localhost:5000',
         changeOrigin: true,
       },
       '/socket.io': {
-        target: 'https://rentdirect-dev.onrender.com',
+        target: 'http://localhost:5000',
         ws: true,
       },
     },
